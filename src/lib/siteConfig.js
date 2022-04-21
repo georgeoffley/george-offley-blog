1521e29ad61c33b3625c9ef6abc84dd601f6756d
George Offley <geo.offley@gmail.com>
2022-04-21 17:40:28 -0400

    updated site config

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,12 +1,12 @@
-export const SITE_URL = 'https://swyxkit.netlify.app';
-export const GH_USER_REPO = 'sw-yx/swyxkit'; // used for pulling github issues and offering comments
+export const SITE_URL = 'https://superlative-dango-d825e3.netlify.app';
+export const GH_USER_REPO = 'georgeoffley/george-offley-blog'; // used for pulling github issues and offering comments
 export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
-export const SITE_TITLE = 'SwyxKit';
-export const SITE_DESCRIPTION = "swyx's default SvelteKit + Tailwind starter";
+export const SITE_TITLE = 'George Offley Blog';
+export const SITE_DESCRIPTION = "George Offley's version of swyx's default SvelteKit + Tailwind starter";
 export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
-export const MY_TWITTER_HANDLE = 'swyx';
-export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
+export const MY_TWITTER_HANDLE = 'georgeoffley';
+export const MY_YOUTUBE = 'https://www.youtube.com/c/GeorgeOffley';
 
 // dont forget process.env.GH_TOKEN
 // if supplied, raises rate limit from 60 to 5000
